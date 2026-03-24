5c3afd6b465295040da82816b3e6895d0e07d510
user <guilherme-siqueira2121@users.noreply.github.com>
2026-03-24 14:34:07 -0300

    'script.js' removed

--- src/script.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll('a').forEach(function(link) {
-    link.addEventListener('click', function(event) {
-        event.preventDefault();
-        var url = this.getAttribute('href');
-        window.location.href = url;
-    });
-})
